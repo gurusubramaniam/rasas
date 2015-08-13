adc7cf6cc1bfd20735470ba04107ffed21967eca
ssubramaniam <ssubramaniam@paypal.com>
2015-08-12 23:52:14 -0700

    chnages for templates and components

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var middleWare = require('../middleware/middleWare');
 
 module.exports = function (router) {
     router({path: '/'}).get(middleWare.renderView);
-    router({path: '/home'}).get(middleWare.renderView);
+    router({path: '/menu'}).get(middleWare.renderView);
     router({path: '/services'}).get(middleWare.renderView);
     router({path: '/feedBack'}).get(middleWare.renderView);
     router({path: '/contactUs'}).get(middleWare.renderView);
