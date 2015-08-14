4e18effca36f7678abe711894701436715ae4972
ssubramaniam <ssubramaniam@paypal.com>
2015-08-14 14:45:39 -0700

    commit

--- middleware/middleWare.js
+++ middleware/middleWare.js
@@ -8,8 +8,18 @@ exports.renderView = function (req, res, next) {
     var model = new IndexModel(),
         viewName = req && req.route && req.route.path || 'index';
 
-    viewName=viewName.replace("/", '');
+    viewName = (viewName === '/') ? 'index' : viewName.replace('/','');
+
     console.log('req path :  ', req.route.path);
     console.log('ViewName :  ', viewName);
     res.render(viewName, model);
-}
+};
+//exports.returnJSON = function (req, res, next) {
+//    res.json(req.model);
+//};
+//
+//exports.menuItems = function (req, res, next) {
+//    var menu = ['Home', 'Menu', 'Feed Back'];
+//    req.model.data.menu = menu;
+//    next();
+//}
